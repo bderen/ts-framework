c9a1c75557291d49cd032db5bad1d94deaae1851
Bogdan Deren <b.deren@mediahuis.nl>
2020-02-11 16:48:36 +0100

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
 const root = document.getElementById('root')
-const user = User.buildUser({ name: 'Name', age: 20 })
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+	return User.buildUser(json)
+})
 
-if (root) {
-	const userEdit = new UserEdit(
-		root,
-		user
-	)
+users.on('change', () => {
+	new UserList(root, users).render()
+})
+
+users.fetch()
+
+// if (root) {
+// 	const userEdit = new UserEdit(
+// 		root,
+// 		user
+// 	)
 	
-	userEdit.render()
-} else {
-	throw new Error('no root selector')
-}
+// 	userEdit.render()
+// } else {
+// 	throw new Error('no root selector')
+// }
 
